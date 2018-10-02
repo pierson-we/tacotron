59a1bf34c16c4c71219dac2008d5d250d57e7c8e
pierson-we <pierson.we@gmail.com>
2018-10-02 15:40:36 -0700

    slack file upload

--- train.py
+++ train.py
@@ -120,7 +120,7 @@ def train(log_dir, args):
           plot.plot_alignment(alignment, os.path.join(log_dir, 'step-%d-align.png' % step),
             info='%s, %s, %s, step=%d, loss=%.5f' % (args.model, commit, time_string(), step, loss))
           log('Input: %s' % sequence_to_text(input_seq))
-          if args.slack_url:
+          if args.slack_url != '':
             cmd = 'curl -F file=@%s -H "Authorization: Bearer xoxb-447699810339-447680344836-l9MSOu5h1NgPambAgN7tFnjo" -F channels=#tensorflow https://slack.com/api/files.upload' % os.path.join(log_dir, 'step-%d-audio.wav')
             p = subprocess.Popen(cmd, shell=True)
             p.wait()
